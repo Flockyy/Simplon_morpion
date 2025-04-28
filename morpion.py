7d77652431a8c1058125402a3c4fb484a37d98b7
RemiVander <rm.vanderperre@gmail.com>
2025-04-28 16:28:54 +0200

    test jouer coup

--- morpion.py
+++ morpion.py
@@ -1,6 +1,8 @@
 # morpion game
 
 array = [[' ' for _ in range(3)] for _ in range(3)]
+joueur_x = "X"
+joueur_o = "O"
 
 print(array)
 
@@ -8,3 +10,10 @@ def print_board():
     for row in array:
         print('|'.join(row))
         print('-' * 5)
+
+def jouer_coup(axe_x, axe_y, joueur):
+    for i in range(array):
+            if axe_x == i:
+                for j in range(array(i)):
+                    if j == axe_y:   
+                        array(j) = joueur
