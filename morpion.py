c81844f040d48ab78bcefbbf225c88022d8b2b11
Florian <florian.abgrall750@gmail.com>
2025-04-28 16:33:34 +0200

    feat: morpion

--- morpion.py
+++ morpion.py
@@ -1,10 +1,20 @@
 # morpion game
 
 array = [[' ' for _ in range(3)] for _ in range(3)]
+
 joueur_x = "X"
 joueur_o = "O"
 
-print(array)
+def check_valid_move(x, y):
+    if x < 0 or x > 2 or y < 0 or y > 2:
+        print("Invalid move: out of bounds")
+        return False
+    if array[x][y] != ' ':
+        print("Invalid move: cell already occupied")
+        return False
+    print("Valid move")
+    return True
+
 
 def print_board():
     for row in array:
@@ -17,3 +27,6 @@ def jouer_coup(axe_x, axe_y, joueur):
                 for j in range(array(i)):
                     if j == axe_y:   
                         array(j) = joueur
+
+print_board()
+check_valid_move(4, 4)
